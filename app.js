4339e4da66f1ce6e8dacdecc5ca034795023be0c
Ariel Alejandro Berurena <arielb991@gmail.com>
2022-11-01 23:33:14 -0300

    revisar efecto lupa

---
+++ app.js
@@ -0,0 +1,41 @@
+//<title>zoom</title>
+                 //     <style> type="text/css" </style>
+             //         <style type="text/css"</style></style>
+               //       .img1 .img2{
+                 //       position: absolute;
+                   //     top: 0px;
+                     //   left: 0px;
+                       // border: 0px;
+                 //       overflow: hidden;
+             //           display: block;
+               //     }
+               //     .img2{
+              //          clip-path: rect(0px, 100px, 100px, 0px) ;
+            //        }
+                      
+
+
+
+
+//**document.onmousemove=ver;
+
+//function ver(e){
+  //  var x, y, x1, y1, x2, y2;
+    //fact= 800/400;
+    //opp= 100;
+
+   // if(e ==null) e=window.event;
+  //  x= e.clientX;
+  //  y= e.clientY;
+
+    //x1=opp-(x)*fact;
+    //y1=opp-(y)*fact;
+    //x2=opp+(x)*fact;
+    //y2=opp+(y)*fact;s
+    
+  //  document.images.big.style.display= "inline";
+ //   document.images.big.style.left= (x)* (1-fact);
+//    document.images.big.style.top= (y)* (1-fact);
+ //   document.images.big.style.clip= "rect("+y1+"px", +x2+"px",+y2+"px", x1+"px)";//** */
+
+
